9250d65d010beb146170c5553174e710d3152c74
Itz-Shadman <chowdhuryshadman707@gmail.com>
2025-10-24 06:55:16 +0100

    third commit

--- src/Pages/ForgotPassword.jsx
+++ src/Pages/ForgotPassword.jsx
@@ -1,4 +1,4 @@
-// src/Pages/ForgotPassword.jsx
+
 import React, { useState } from "react";
 import { sendPasswordResetEmail } from "firebase/auth";
 import { auth } from "../Firebase/Firebase.config";
